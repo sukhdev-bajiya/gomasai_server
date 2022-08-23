f921fc972601ac88e9631a41b8d434e4e1e42928
sukhdev-bajiya <bajiya4442@gmail.com>
2022-08-23 10:43:22 +0530

    server format done

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const items = require("./data/items");
 const tags = require("./data/tags");
 const brands = require("./data/brands");
-const itemTypes = require("./data/gomasai");
+const gomasai = require("./data/gomasai");
 const dev = require("./data/dev");
 
 module.exports = {
